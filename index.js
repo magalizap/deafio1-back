8f69eb1910aea61f5b8337e49c0ea5b9c30c52c4
Magali Sol <magalisol2307@gmail.com>
2023-04-08 19:21:05 -0300

    nuevo commit

--- index.js
+++ index.js
@@ -1,38 +1,63 @@
-// PRIMER DESAFÍO: 
+// PRIMER Y SEGUNDO DESAFÍO: 
+
+import {promises as fs} from 'fs'
 
 class ProductManager {
-    constructor(){
+    constructor(path){
+        this.path = path
         this.products = [], // estado inicial del carrito
         this.id = 1
     }
-    addProduct(product){
+    async addProduct(product){
         for (let i = 0; i < this.products.length; i++) { // Validación campo code
             if(this.products[i].code === product.code){
-                console.log(`El código ${product.code} hace referencia a un producto existente`);
-                break;
+                console.log(`El código ${product.code} hace referencia a un producto existente`)
+                return
             }
-        }
+        } 
 
         if(!Object.values(product).includes(undefined)){ // corroboramos que no haya campos vacíos
             product.id = this.id ++
-            this.products.push(product) //agregamos los productos al arreglo
+            this.products.push(product) // agregamos productos dentro de mi array
+            await fs.writeFile(this.path, JSON.stringify(this.products, null, 2)) // creo y actualizo mi array de productos en un txt
         }else{
             console.log('Debes llenar todos los campos')
         }
-
-
     }
 
-    getProduct(){
-        return this.products // nos devuelve el arreglo con los productos creados
+    
+    async getProduct(){
+        const readProduct = await fs.readFile(this.path, 'utf-8') // consulto por el array creado en el txt
+        const prods = JSON.parse(readProduct)
+        console.log(prods)
     }
 
-    getProductById(id){
-        if(!this.products.find((product) => product.id === id)){ // buscamos un producto por su id 
-            console.log('Not found')
-        }else{
-            console.log(this.products.find((product) => product.id === id))
+    async getProductById(id){
+        const readProduct2 = await fs.readFile(this.path, 'utf-8') 
+        const prods = JSON.parse(readProduct2)
+        let aux = prods.find((product) => product.id === id)
+        aux ? console.log(aux) : console.log('Not Found')
+        await fs.writeFile(this.path, JSON.stringify(aux))
+    }
+    async updateProduct(id, updates){
+        const readProduct3 = await fs.readFile(this.path, 'utf-8') 
+        const prods = JSON.parse(readProduct3)
+        const productIndex = prods.findIndex((product) => product.id === id)
+        if(productIndex === -1){
+            console.log('Product not found')
+            return
         }
+        const updateProduct = Object.assign({}, prods[productIndex], updates)
+        prods[productIndex] = updateProduct
+        await fs.writeFile(this.path, JSON.stringify(prods))
+        console.log(prods)
+    }
+    async deleteProduct(id){
+        const readProduct4 = await fs.readFile(this.path, 'utf-8') 
+        const prods = JSON.parse(readProduct4)
+        const aux = prods.filter(products => products.id !== id)
+        await fs.writeFile(this.path, JSON.stringify(aux))
+        console.log(aux)
     }
 }
 
@@ -53,16 +78,24 @@ const product2 = new Product('Oreo', 'Galletitas 354gr', 555, 'https://jumboarge
 
 const product3 = new Product('Playadito Suave', 'Yerba Mate X1kg', 679, 'https://jumboargentina.vtexassets.com/arquivos/ids/711224-800-auto?v=637938633804770000&width=800&height=auto&aspect=true', 'SKU: 11230376006', 236)
 
-const productManager = new ProductManager() 
-// utilizando la constante productManager llamamos al método para agregar los productos
-productManager.addProduct(product1)
-productManager.addProduct(product2)
-productManager.addProduct(product3)
+const product4 = new Product('Cachafaz', 'Galletitas Integral Granola', 410, 'https://jumboargentina.vtexassets.com/arquivos/ids/192618-800-auto?v=636383534226100000&width=800&height=auto&aspect=true', 'SKU: 11121604001', 350)
+//const product5 = new Product()
 
-console.log(productManager.getProduct())
+const productManager = new ProductManager('./products.txt') 
+
+// utilizando la constante productManager llamamos al método para agregar los productos
+await productManager.addProduct(product1)
+await productManager.addProduct(product2)
+await productManager.addProduct(product3)
+await productManager.addProduct(product4)
+
 
 // prueba code repetido 
-const product4 = new Product('Playadito Suave', 'Yerba Mate X1kg', 679, 'https://jumboargentina.vtexassets.com/arquivos/ids/711224-800-auto?v=637938633804770000&width=800&height=auto&aspect=true', 'SKU: 11230376006', 236)
+const product6 = new Product('Playadito Suave', 'Yerba Mate X1kg', 679, 'https://jumboargentina.vtexassets.com/arquivos/ids/711224-800-auto?v=637938633804770000&width=800&height=auto&aspect=true', 'SKU: 11230376006', 236)
+//productManager.addProduct(product6) // producto con code repetido
 
-productManager.getProductById(2) // buscador de productos por id
-productManager.addProduct(product4) 
+
+//productManager.getProductById(4) // buscador de productos por id
+//productManager.updateProduct(product2.id, { price: 690, description: "Galletitas Oreo Bañadas Chocolate Leche Milka 119g Capsuland" }) //actualiza campos del producto
+//productManager.deleteProduct(3)
+//await productManager.getProduct()
